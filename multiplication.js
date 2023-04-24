b8b07c61aa768f70f84ff8de5878d24bb9c47725
Rachanaaaa1 <117339694+Rachanaaaa1@users.noreply.github.com>
2023-04-24 23:35:34 +0530

    Update multiplication.js
    updated

--- multiplication.js
+++ multiplication.js
@@ -2,4 +2,4 @@ const Fraction = require('fraction.js);
 const frac1 = new Fraction(3,4);
 const frac2 = new Fraction(1,2);
 const result = frac1.sub(frac2);
-console.log(`The result of subtracting ${frac2} from ${frac1} is: ${result}`);
+console.log(`The result of subtracting ${frac2} from ${frac1} is: ${result}`); 
