560247e8033e3187e09a29e408824814e5643e73
Rachanaaaa1 <117339694+Rachanaaaa1@users.noreply.github.com>
2023-04-24 23:34:53 +0530

    Create multiplication.js

---
+++ multiplication.js
@@ -0,0 +1,5 @@
+const Fraction = require('fraction.js);
+const frac1 = new Fraction(3,4);
+const frac2 = new Fraction(1,2);
+const result = frac1.sub(frac2);
+console.log(`The result of subtracting ${frac2} from ${frac1} is: ${result}`);
